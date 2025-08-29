8b7ad15571f5f04ef5203a784fa3d3fee9cb8a1d
HarrisonMiqueias <106624984+HarrisonMiqueias@users.noreply.github.com>
2025-08-28 21:49:19 -0300

    Atualizar o GatherLite.js

--- src/GatherLite.js
+++ src/GatherLite.js
@@ -27,7 +27,7 @@ const SOCKET_SERVER = process.env.REACT_APP_SOCKET_SERVER || "http://localhost:5
   // -------------------- PEGAR MÍDIA LOCAL --------------------
   useEffect(() => {
     navigator.mediaDevices
-      .getUserMedia({ video: true, audio: true })
+      .getUserMedia({ video: false, audio: true })
       .then((stream) => {
         localStreamRef.current = stream;
         if (localVideoRef.current) localVideoRef.current.srcObject = stream;

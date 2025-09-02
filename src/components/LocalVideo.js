24cffbbedb1c7b40a5c356416dac66653970da28
Harrison <harrisonmiqueias@gmail.com>
2025-09-02 09:07:26 -0300

    te

--- src/components/LocalVideo.js
+++ src/components/LocalVideo.js
@@ -40,7 +40,7 @@ export default function LocalVideo({
     if (videoTrack) {
       videoTrack.stop();
       localStreamRef.current.removeTrack(videoTrack);
-      setVideoEnabled(false);
+      setVideoEnabled(true);
       if (localVideoRef.current) {
         localVideoRef.current.srcObject = new MediaStream(localStreamRef.current.getTracks());
       }

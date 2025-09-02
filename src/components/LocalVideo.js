47174fafd23610a2a2175932bf283de1d64db211
Harrison <harrisonmiqueias@gmail.com>
2025-09-02 09:34:21 -0300

    teste

--- src/components/LocalVideo.js
+++ src/components/LocalVideo.js
@@ -142,8 +142,7 @@ export default function LocalVideo({
         </button>
 
         <button
-         onClick={videoEnabled ? disableVideo : enableVideo}
-          //onClick={toggleAudio}
+          onClick={toggleAudio}
           style={{
             padding: "6px 12px",
             backgroundColor: audioEnabled ? "#22c55e" : "#ef4444",
